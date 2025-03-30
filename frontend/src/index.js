8aef52916932cc01546b79af7ab701eaf132b3b9
kylet-01 <userEmail>
2025-03-30 20:54:59 +1100

    wrap app in global context provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { GlobalContextProvider } from './context/GlobalContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalContextProvider>
+      <App />
+    </GlobalContextProvider>
+
   </React.StrictMode>
 );
 
