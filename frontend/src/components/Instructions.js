7e25bbd5154044cb675ee667f25b44e69cfe16cb
kylet-01 <userEmail>
2025-03-31 14:13:44 +1100

    updated instructions to say that chords are not currently implemented

--- frontend/src/components/Instructions.js
+++ frontend/src/components/Instructions.js
@@ -10,12 +10,16 @@ const Instructions = () => {
             <ul>
                 <li>Single Notes: C</li>
                 <li>Accidentals: b, # (for example: C#)</li>
+                <li>Chaining Notes: use - to chain notes and chords to be played at the same time (for example: D-F#-A)</li>
+                <li>duration (optional): use : to specify duration (for example: C:1/4 specifies quarter note, default duration is 1/4 if omitted)</li>
+                <li>No Note: . to specify no note to be played (for example: .: or just .) </li>
+                <li>Octave (optional, range = [1,6]): specify octave a note that will be played in, default is 4 (for example: C4, default octave is 4 if omitted) </li>
+            </ul>
+            <h1> Features to be implemented </h1>
+            <ul>
                 <li>Chords: maj, min, dim, aug (for example: Cmaj)</li>
                 <li>7th Chords: maj7, min7, dim7, dom7 (for example: Cmin7)</li>
-                <li>Chaining Notes: use - to chain notes and chords to be played at the same time (for example: D-F#-A)</li>
-                <li>duration (optional): use : to specify duration (for example: Cmaj:1/4 specifies quarter note, default duration is 1/4 if omitted )</li>
-                <li>No Note: . to specify no note to be played (for example: .:1/2) </li>
-                <li>Octave (optional, range = [1,6]): specify octave a note that will be played in, default is 4 (for example: C#4maj7, default octave is 4 if omitted) </li>
+                <li>Complex chord example: C4maj-B4:1/2</li>
             </ul>
             <h1> Example chord progressions</h1>
             <ul>
