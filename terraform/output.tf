ba1798f6c2c6d3f45d6e5e1ee1cbacdc16e1757c
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-04-28 17:22:20 +1000

    add output.tf to track outputs

---
+++ terraform/output.tf
@@ -0,0 +1,17 @@
+# lf url
+output "lambda_function_url" {
+  description = "URL to invoke the Lambda function directly"
+  value       = aws_lambda_function_url.lf_url.function_url
+}
+
+# name of db
+output "dynamodb_table_name" {
+  description = "The DynamoDB table name"
+  value       = aws_dynamodb_table.db.name
+}
+
+# url of frontend
+output "s3_website_url" {
+  description = "URL of the S3 static website hosting"
+  value       = "http://${aws_s3_bucket.app.website_endpoint}"
+}
