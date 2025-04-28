a7c2be1c63ee47980b707ee0f3a808eb4739b489
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-04-28 22:01:34 +1000

    replace placeholder bucket name with actual s3 bucket name

--- terraform_apply.sh
+++ terraform_apply.sh
@@ -23,6 +23,9 @@ echo "REACT_APP_LF_REPORT=$(terraform output -raw lambda_function_url)" > ../fro
 # get URL
 URL=$(terraform output -raw s3_website_url)
 
+# bucket name
+BUCKET=${terraform output -raw s3_bucket_name}
+
 # now go to root directory
 cd ..
 
@@ -31,9 +34,12 @@ echo "Updating/Replacing README.md and cicd.yml App URL"
 # automaticaly updating README.md
 sed -i "s|(http[^)]*)|\($URL\)|g" ./README.md
 
-# now update the cicd environment variables
+# now update the cicd environment variables 
+# update the URL
 sed -i "s|URL: .*|URL: $URL|g" .github/workflows/cicd.yml
 
+# update the aws s3 bucket name
+sed -i "s|BUCKET_NAME: .*|BUCKET_NAME: s3://$BUCKET|g" .github/workflows/cicd.yml
 
 echo "###END###"
 
