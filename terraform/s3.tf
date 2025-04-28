42898b1098490faa7f4df11daab38e5df11a4de1
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-04-28 18:10:51 +1000

    policy added after allowing public access

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,29 +1,29 @@
 # static website bucket, currently uses existing s3 bucket
 resource "aws_s3_bucket" "app" {
-    bucket = "parse-and-play-2"
-    force_destroy = true
+  bucket        = "parse-and-play-2"
+  force_destroy = true
 
-    depends_on = [
-    aws_s3_bucket_public_access_block.app
-    ]
-
-    website {
-        index_document = "index.html"
-    }
+  website {
+    index_document = "index.html"
+  }
 }
 
 # allow public access
 resource "aws_s3_bucket_public_access_block" "app" {
-    bucket = aws_s3_bucket.app.id
-    block_public_acls   = false
-    block_public_policy = false
-    ignore_public_acls  = false
-    restrict_public_buckets = false
+  bucket                  = aws_s3_bucket.app.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 # public read policy
 resource "aws_s3_bucket_policy" "app_public_read" {
   bucket = aws_s3_bucket.app.id
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.app
+  ]
 
   policy = jsonencode({
     Version = "2012-10-17",
