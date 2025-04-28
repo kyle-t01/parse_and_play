dfd14478b93226fb696a3e6c7c5f23f2b27718f5
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-04-28 22:01:00 +1000

    output the s3 bucket name

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,6 +1,6 @@
 # lf url
 output "lambda_function_url" {
-  description = "Lambda function URL directly"
+  description = "Lambda function URL"
   value       = aws_lambda_function_url.lf_url.function_url
 }
 
@@ -8,4 +8,10 @@ output "lambda_function_url" {
 output "s3_website_url" {
   description = "URL of the S3 static website hosting"
   value       = "http://${aws_s3_bucket_website_configuration.app_website.website_endpoint}"
+}
+
+# S3 bucket name
+output "s3_bucket_name" {
+  description = "S3 bucket name"
+  value       = aws_s3_bucket.app.bucket
 }
