f0c22ecbb9cd483aca2eeeee307adb24cdc0e30a
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-04-28 16:42:24 +1000

    lambda function now allows URL of the newly created s3 bucket

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,21 +1,21 @@
 # lambda funtions
 resource "aws_lambda_function" "lf" {
-  function_name = "parse_and_play_lf"
-  handler       = "index.handler"
-  runtime       = "nodejs22.x"
-  role          = aws_iam_role.lf_iam.arn
+  function_name    = "parse_and_play_lf"
+  handler          = "index.handler"
+  runtime          = "nodejs22.x"
+  role             = aws_iam_role.lf_iam.arn
   filename         = data.archive_file.lf_zip.output_path
   source_code_hash = data.archive_file.lf_zip.output_base64sha256
 }
 
 # url and config
 resource "aws_lambda_function_url" "lf_url" {
-    function_name = aws_lambda_function.lf.function_name
-    authorization_type = "NONE"
-    cors {
-        allow_origins     = ["http://parse-and-play.s3-website-ap-southeast-2.amazonaws.com"]
-        allow_methods     = ["GET", "POST"]
+  function_name      = aws_lambda_function.lf.function_name
+  authorization_type = "NONE"
+  cors {
+    allow_origins     = ["http://${aws_s3_bucket.app.bucket_regional_domain_name}"]
+    allow_methods     = ["GET", "POST"]
     allow_headers     = ["content-type"]
     allow_credentials = false
-    }
+  }
 }
